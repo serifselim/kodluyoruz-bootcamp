bd8e2de2af8ede04d5e43c2ae3638cd8f72dc7f4
Yavuz Selim Şerifoğlu <70685127+serifselim@users.noreply.github.com>
2022-06-26 22:55:05 +0300

    comment lines add

---
+++ medium-odev/index.js
@@ -0,0 +1,32 @@
+/* Function Types */
+
+// Function Declaration
+
+// function functionName(p1, p2) {
+//     return p1 + p2;
+// }
+
+// Function Expression
+
+// functionName(20, 30); // Output : 50
+
+// const newVar = function functionName(p1, p2) {
+//     return p1 + p2;
+// };
+
+// newVar(20, 30); // Output : 50
+
+// Arrow Function 
+
+// const newVar = (p1, p2) => {
+//     let calc = p1 + p2;
+//     return calc;
+// };
+
+// newVar(20,50) // Output : 50
+
+const newVar = (p1, p2) => p1 + p2;
+
+newVar(20, 50); // Output : 50
+
+
